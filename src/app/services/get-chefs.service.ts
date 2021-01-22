70dc693bdaa69ff6278b86046a743c5fb7c8c18e
Brandon Zukowski <bzukowski@atb.com>
2021-01-22 15:03:42 -0700

    deploy to heroku

--- src/app/services/get-chefs.service.ts
+++ src/app/services/get-chefs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators'
