255bc81fcd5548862c1e88771834166fbbf3802f
Brandon Zukowski <bzukowski@atb.com>
2021-01-23 23:57:11 -0700

    added shuffle

--- src/app/services/chef-teams.service.ts
+++ src/app/services/chef-teams.service.ts
@@ -18,6 +18,8 @@ export class ChefTeamsService {
   generateChefTeams(numberOfTeams: number) {
     return new Promise<any>((resolve, reject) => {
       this.getChefsSvc.getChefs().subscribe(chefs => {
+        // we shuffle the order of chefs in order to get randomized teams every time we generate new teams
+        this.randomizeChefs(chefs)
         // calculate total number of players required to make all teams have the same number of players
         // eg. if 39 players need to be split into 5 teams, then the most players we can use is 35
         let totalPlayers = chefs.length - (chefs.length % numberOfTeams)
@@ -68,6 +70,31 @@ export class ChefTeamsService {
     // return the remaining chefs at the end of the list 
     // that were not assigned to a kitchen team
     return chefs.slice(totalPlayers, chefs.length)
+  }
+
+  randomizeChefs(chefs: Chef[]) {
+    // to shuffle the array we will use the Fisher-Yates (aka Knuth) un-biased shuffle algorithm.
+    // more information can be found about the algorithm here
+    // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+    // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    let currentIndex = chefs.length
+    let temporaryValue
+    let randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = chefs[currentIndex];
+      chefs[currentIndex] = chefs[randomIndex];
+      chefs[randomIndex] = temporaryValue;
+    }
+
+    return chefs;
   }
 
   clearChefTeams() {
